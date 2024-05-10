9920c85b0efdef115c6faf17cf746142a1087aa7
GauravKesh <gkrcoder@gmail.com>
2024-05-10 11:57:51 +0530

    new question

--- Strings/Rotate String.cpp
+++ Strings/Rotate String.cpp
@@ -11,11 +11,17 @@ public:
 	static bool rotateString(string s, string goal) {
 		int j = 0;
 		int n = s.length();
+		// if len !=  then return false;
 		if (n > goal.length()) return false;
+
+		// iterate through each char
 		for (int i = 0; i < n; i++) {
+			//push first in last
 			s.push_back(s[i]);
+			// check from ith+1 position to end it giving string or not
 			if (s.substr(i + 1) == goal) return true;
 		}
+		// check from begining...
 		return s.substr(j) == goal;
 	}
 };
