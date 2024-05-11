82515d288cfb782ebaf3d08588733110906fbd25
GauravKesh <gkrcoder@gmail.com>
2024-05-11 11:04:00 +0530

    new question

--- Strings/Check if two strings are anagram of each other.cpp
+++ Strings/Check if two strings are anagram of each other.cpp
@@ -3,9 +3,9 @@
 //
 #include<iostream>
 #include<string>
+#include <vector>
 
 using namespace std;
-
 
 
 class Solution {
@@ -18,12 +18,39 @@ public:
 
 	}
 };
+/*Sorting both strings has a time complexity of O((n+m)log(n+m)),
+ which could be inefficient for large strings.*/
+
+/*to optimise this we'll be using hashmap method*/
+
+class SolutionOptimal {
+public:
+	static bool isAnagram(string &s, string &t) {
+		if (s.length() != t.length()) {
+			return false;
+		}
+		vector<int> cnt(26, 0);
+		for (char c: s) {
+			cnt[c - 'a']++;
+
+		}
+		for (char c: t) {
+			cnt[c - 'a']--;
+		}
+		for (int c: cnt) {
+			if (c != 0)
+				return false;
+		}
+		return true;
+	}
+};
 
 
 int main() {
 	string s = "anagram";
 	string t = "nagaram";
-	bool k = Solution::isAnagram(s, t);
+	bool k = SolutionOptimal::isAnagram(s, t);
+
 	cout << k;
 }
 
