64739c43188e96599f928233acdcb0b016801ac6
GauravKesh <gkrcoder@gmail.com>
2024-05-13 20:28:34 +0530

    new question

--- Strings/Medium/Sort Characters By Frequency.cpp
+++ Strings/Medium/Sort Characters By Frequency.cpp
@@ -1,3 +1,26 @@
 //
 // Created by Gaurav Kesh Roushan on 12/05/24.
 //
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+
+class Solution {
+public:
+	static string frequencySort(string s) {
+		sort(s.begin(),s.end());
+		return s;
+	}
+};
+
+
+int main(){
+	string  s= "AAbbbb";
+	string k = Solution::frequencySort(s);
+			cout<<k;
+}
