80fe1dc2f4ab2a79df423510dbd72dcb1c755cb2
GauravKesh <gkrcoder@gmail.com>
2024-05-16 23:34:56 +0530

    new question

---
+++ Strings/Medium/ Implement Atoi.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Gaurav Kesh Roushan on 16/05/24.
+//
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+
+
+
+int main() {
+	std::string s = "III";
+	std::string s1 = "LVIII";
+	std::string s2 = "MCMXCIV";
+
+	int n = Solution::romanToInt(
+			s2);
+	int n1 = SolutionOptimal::romanToInt(
+			s2);
+	std::cout << n<<std::endl;
+	std::cout << n1<<std::endl;
