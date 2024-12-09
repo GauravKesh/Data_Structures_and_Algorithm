38fe51adba3ca0ab0bb02154123dfa09cd99660f
GauravKesh <gkrcoder@gmail.com>
2024-12-09 14:01:13 +0530

    Leetcode problems

--- Strings/28. Find the Index of the First Occurrence in a String.cpp
+++ Strings/28. Find the Index of the First Occurrence in a String.cpp
@@ -1,10 +1,30 @@
 //
 // Created by Gaurav Kesh Roushan on 09/12/24.
 //
-class Solution {
+
+/*using builtin methods*/
+class Solution1 {
 public:
 	int strStr(string haystack, string needle) {
 		int ind =-1; // index of first occurance
 		return haystack.find(needle);
+	}
+};
+
+/*Making own function to check*/
+class Solution2 {
+public:
+	int strStr(string haystack, string needle) {
+		if (haystack.length() < needle.length()) {
+			return -1;
+		}
+
+		for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+			if (haystack.substr(i, needle.length()) == needle) {
+				return i;
+			}
+		}
+
+		return -1;
 	}
 };
