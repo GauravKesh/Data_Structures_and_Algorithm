0052a967341569d915da5435f993e724e8ad2360
GauravKesh <gkrcoder@gmail.com>
2025-01-07 19:25:35 +0530

    Leetcode problems

---
+++ Binary_Search_Tree/700_Search in a Binary_Search_Tree.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Gaurav Kesh Roushan on 07/01/25.
+//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* searchBST(TreeNode* root, int val) {
+		if(root!=nullptr){
+			if(root->val==val){
+				return root;
+			}else if(root->val>val){
+				return searchBST(root->left,val);
+			}else{
+				return searchBST(root->right,val);
+			}
+
+		}
+		return nullptr;
+	}
+};
