11541279e0cd1a37e375ed5b3e40051e6cf141f5
GauravKesh <gkrcoder@gmail.com>
2025-02-01 22:21:18 +0530

    Stack and Queue USING LINKED lIST

--- Stack_Queue/Queue_using_linkedlist.cpp
+++ Stack_Queue/Queue_using_linkedlist.cpp
@@ -1,3 +1,108 @@
 //
 // Created by Gaurav Kesh Roushan on 01/02/25.
 //
+
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node *next;
+
+	Node(int dat) {
+		this->data = dat;
+		this->next = nullptr;
+	}
+};
+
+class QueueLL {
+private:
+	int size;
+	Node *start;
+	Node *end;
+
+public:
+	QueueLL() {
+		this->start = nullptr;
+		this->end = nullptr;
+		this->size = 0;
+	}
+
+	bool isQueueEmpty() {
+		return start == nullptr;
+	}
+
+	void push(int val) {
+		Node* temp = new Node(val);
+		if (isQueueEmpty()) {
+			start = temp;
+			end = temp;
+		} else {
+			end->next = temp;
+			end = temp;
+		}
+		size++;
+	}
+
+	void pop() {
+		if (isQueueEmpty()) {
+			cout << "Queue is Empty" << endl;
+			return;
+		}
+		Node* temp = start;
+		start = start->next;
+		delete temp;
+		size--;
+
+		if (start == nullptr) {
+			end = nullptr; // Reset end when queue becomes empty
+		}
+	}
+
+	int peek() {
+		if (isQueueEmpty()) {
+			cout << "Queue is Empty" << endl;
+			return -1;
+		}
+		return start->data;
+	}
+
+	void display() {
+		if (isQueueEmpty()) {
+			cout << "Queue is Empty" << endl;
+			return;
+		}
+		Node *temp = start;
+		cout << "Queue Elements: ";
+		while (temp != nullptr) {
+			cout << temp->data << " <- ";
+			temp = temp->next;
+		}
+		cout << "NULL" << endl;
+	}
+
+	~QueueLL() {
+		while (!isQueueEmpty()) {
+			pop();
+		}
+	}
+};
+
+int main() {
+	QueueLL queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+
+	queue.display(); // Output: Queue Elements: 10 <- 20 <- 30 <- NULL
+
+	cout << "Front of Queue: " << queue.peek() << endl; // Output: Front of Queue: 10
+
+	queue.pop();
+	queue.display(); // Output: Queue Elements: 20 <- 30 <- NULL
+
+	return 0;
+}
--- Stack_Queue/Stack_Using_LinkedList.cpp
+++ Stack_Queue/Stack_Using_LinkedList.cpp
@@ -2,9 +2,95 @@
 // Created by Gaurav Kesh Roushan on 01/02/25.
 //
 #include <iostream>
+
 using namespace std;
 
+class Node {
+public:
+	int data;
+	Node *next;
 
+	Node(int dat) {
+		this->data = dat;
+		this->next = nullptr;
+	}
+};
 
+class StackLL {
+private:
+	Node* top;
 
+public:
+	StackLL() {
+		this->top = nullptr;
+	}
 
+	bool isStackEmpty() {
+		return top == nullptr;
+	}
+
+	void push(int val) {
+		Node *node = new Node(val);
+		if (top == nullptr) {
+			top = node;
+		} else {
+			node->next = top;
+			top = node;
+		}
+	}
+
+	void pop() {
+		if (isStackEmpty()) {
+			cout << "Stack underflow! Cannot pop from an empty stack." << endl;
+			return;
+		}
+		Node *temp = top;
+		top = top->next;
+		delete temp;
+	}
+
+	int peek() {
+		if (isStackEmpty()) {
+			cout << "Stack is empty!" << endl;
+			return -1; // Return a sentinel value
+		}
+		return top->data;
+	}
+
+	void display() {
+		if (isStackEmpty()) {
+			cout << "Stack is empty!" << endl;
+			return;
+		}
+		Node *temp = top;
+		cout << "Stack elements: ";
+		while (temp != nullptr) {
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+		cout << endl;
+	}
+
+	~StackLL() {
+		while (!isStackEmpty()) {
+			pop();
+		}
+	}
+};
+
+int main() {
+	StackLL stack;
+
+	stack.push(10);
+	stack.push(20);
+	stack.push(30);
+
+	stack.display(); // Output: Stack elements: 30 20 10
+
+	cout << "Top element: " << stack.peek() << endl; // Output: Top element: 30
+
+	stack.pop();
+	stack.display(); // Output: Stack elements: 20 10
+
+	return 0;
+}
